cc3d9d1f38f4550a20a12825c425d2518bde71d3
𝖺𝖳𝖺𝗋𝗂 <ssdcv608@gmail.com>
2024-06-05 02:25:24 +0300

    D2

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/JUSTATARI/Quran",
+    "https://github.com/afkhcftjma/Quran",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
